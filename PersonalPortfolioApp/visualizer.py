754917c4ec5f0a16afde6e6798e4a0bbb2f528b0
Logan Berk <146039828+LoganOBerk@users.noreply.github.com>
2026-03-14 17:06:16 -0400

    cleaned up naming

--- PersonalPortfolioApp/visualizer.py
+++ PersonalPortfolioApp/visualizer.py
@@ -2,4 +2,12 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 class Visualizer:
-    pass
+
+    #FUNC DEF: Pie chart display of holdings
+    #PURPOSE: to visualize the holdings distribution
+    @staticmethod
+    def display_pie_chart(raw_holdings_data):
+        #TODO: use pandas to format the data
+        #TODO: use matplotlib to display the data
+        pass
+    
