fc2e25c6a272fb09fa692662d8ea6d2e6855cd10
Logan Berk <146039828+LoganOBerk@users.noreply.github.com>
2026-03-14 17:14:37 -0400

    adjusted integration skeleton

--- PersonalPortfolioApp/integration_layer.py
+++ PersonalPortfolioApp/integration_layer.py
@@ -3,5 +3,10 @@ import yfinance as yf
 class Api:
 
     @staticmethod
-    def get_stock_price(ticker):
+    def get_stock_price(ticker) -> int:
+        #TODO: Pull stock price data given a ticker
+        pass
+    @staticmethod
+    def does_ticker_exist(ticker) -> bool:
+        #TODO: Call the api to identify if the ticker actually exists
         pass
